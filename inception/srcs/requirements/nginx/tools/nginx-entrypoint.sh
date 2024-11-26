55e8d8edb57e0599606d288a77a5a89a0713806e
lumikil <kilpelainenlumi@gmail.com>
2024-11-26 08:46:54 +0200

    Update Makefile and environment configuration for improved service setup and remove unused NGINX configuration

--- inception/srcs/requirements/nginx/tools/nginx-entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -16,6 +16,15 @@ if [ ! -e /etc/.firstrun ]; then
     # Configure nginx to serve static WordPress files and to pass PHP requests
     # to the WordPress container's php-fpm process
     cat << EOF >> /etc/nginx/http.d/default.conf
+
+server {
+    listen 80;
+    server_name $DOMAIN_NAME;
+
+    # Redirect all HTTP requests to HTTPS
+    return 301 https://$server_name$request_uri;
+    }
+
 server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
